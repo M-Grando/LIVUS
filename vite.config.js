aa20fd5dc6ea2e55232e7b042ba2d2fbf55f1182
M-Grando <matheusgrando@gmail.com>
2025-09-27 17:24:37 -0300

    Segundo

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', //
+
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
@@ -12,3 +14,4 @@ export default defineConfig({
     },
   },
 })
+
